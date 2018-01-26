b3cfe56751a0389aa5b53df20bf4128deb135535
Timothy Sullivan <tsullivan@elastic.co>
2018-01-25 22:52:36 -0700

    scroller and flexbox

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,59 +1,82 @@
 /* global $ */
+function Ajax() {
 
-function updateHistory(message, $container) {
-  const html = window.messageFormatter(message);
-  if (html) {
-    $container.prepend(html);
+  function Scroller($container) {
+    // http://jsfiddle.net/dotnetCarpenter/KpM5j/
+    const el = $container.get(0);
+    const { scrollHeight, clientHeight, scrollTop } = el;
+
+    this.trackingBottom = scrollHeight - clientHeight <= scrollTop + 1;
+    this.scroll = () => {
+      if (this.trackingBottom) {
+        const { scrollHeight, clientHeight } = el;
+        el.scrollTop = scrollHeight - clientHeight;
+      }
+    };
+
+    return this;
   }
-}
 
-const AJAX_OPTS = {
-  method: 'post',
-  contentType: 'application/json',
-  url: '/chat',
-  dataType: 'json'
-};
-
-function sendMessage(messageText, messageFormat, callback) {
-  const message = {
-    format: messageFormat,
-    message: messageText,
-    time: Date.now()
-  };
-
-  $.ajax(
-    Object.assign({}, AJAX_OPTS, {
-      data: JSON.stringify(message),
-      success: (response) => callback(message, response)
-    })
-  );
-}
-
-function handleSubmit($userText, $history) {
-  return e => {
-    e.preventDefault();
-
-    const messageText = $userText.val();
-    if (messageText === '') {
-      return false;
+  function updateHistory(message, $container) {
+    const html = window.messageFormatter(message);
+    if (html) {
+      const scroller = new Scroller($container); // capture bottom tracking
+      const $html = $(html);
+      $container.append($html); // add to page
+      $html.fadeTo(0, 0); // make invisible
+      scroller.scroll(); // scroll down
+      $html.fadeTo(1200, 1); // make visible
     }
+  }
 
-    sendMessage(messageText, 'user', (message, response) => {
-      updateHistory(message, $history);
-      $userText.val('');
-      updateHistory(response, $history);
-    });
+  const AJAX_OPTS = {
+    method: 'post',
+    contentType: 'application/json',
+    url: '/chat',
+    dataType: 'json'
+  };
+
+  function sendMessage(messageText, messageFormat, callback) {
+    const message = {
+      format: messageFormat,
+      message: messageText,
+      time: Date.now()
+    };
+
+    $.ajax(
+      Object.assign({}, AJAX_OPTS, {
+        data: JSON.stringify(message),
+        success: (response) => callback(message, response)
+      })
+    );
+  }
+
+  function handleSubmit($userText, $history) {
+    return e => {
+      e.preventDefault();
+
+      const messageText = $userText.val();
+      if (messageText === '') {
+        return false;
+      }
+
+      sendMessage(messageText, 'user', (message, response) => {
+        updateHistory(message, $history);
+        $userText.val('');
+        updateHistory(response, $history);
+      });
+    };
+  }
+
+  return {
+    initAjax($userText, $history) {
+      $('#chat').on('submit', handleSubmit($userText, $history));
+
+      sendMessage('HELO', 'syn', (message, response) => {
+        updateHistory(response, $history);
+      });
+    }
   };
 }
 
-$(document).ready(() => {
-  const $userText = $('#userText');
-  const $history = $('#history');
-  $('#chat').on('submit', handleSubmit($userText, $history));
-
-  sendMessage('HELO', 'syn', (message, response) => {
-    updateHistory(response, $history);
-  });
-
-  $userText.focus();
-});
+window.Ajax = Ajax;
