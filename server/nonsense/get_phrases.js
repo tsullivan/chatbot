d3f9d67c91bcf1e946d7c7b9a55176e0d525d06d
Timothy Sullivan <tsullivan@elastic.co>
2018-02-10 15:23:13 -0700

    fix some phrases

--- server/nonsense/get_phrases.js
+++ server/nonsense/get_phrases.js
@@ -1,5 +1,3 @@
-const { capitalize } = require('lodash');
-
 function getPhrases({ adjective: upperA, noun: upperN }) {
   const proper = `${upperA} ${upperN}`;
   const lower = proper.toLowerCase();
@@ -7,36 +5,38 @@ function getPhrases({ adjective: upperA, noun: upperN }) {
   const lowerN = upperN.toLowerCase();
 
   return [
-    `${capitalize(lower)}. Pass it on.`,
-    `${capitalize(lowerN)} with a cherry on top.`,
+    `My ${upperN} is ${lowerA}.`,
+    `Want a ${lowerN}? How about a ${lowerA} ${lowerN}?`,
     `${proper} for President!`,
-    `A ${proper} Really A ${proper}`,
-    `Aint no ${lowerN} like a ${lowerA} ${lowerN} because a ${lowerA} ${lowerN} don't stop!`,
-    `Are you out there, ${proper}?`,
-    `Are you there, God? It's me, ${proper}.`,
+    `A ${lowerN}‽ Really‽ A ${lower}‽`,
+    `Are you out there, ${lower}s?`,
+    `Are you there? It's me, ${proper}.`,
     `Did you say "${proper}"?`,
+    `Just don't say "${proper}".`,
     `Everything I know I learned from a ${lowerN}.`,
     `Free ${lowerN} with every happy meal!`,
     `I could really go for a nice cold ${lowerN} right about now.`,
-    `I once saw a giant ${lowerN}.`,
-    `I punched it in the ${lowerN}!`,
+    `I once saw a ${lowerN} looking very ${lowerA}.`,
+    `Does your mother know you're talking about ${lower}s?`,
     `I'm feeling ${lowerA}. How about you?`,
-    `I'm not ${lowerA}!`,
-    `If you say "${lowerN}", I'm going to turn ${lowerA}`,
+    `I'm not a ${lower}!`,
+    `If you say "${lowerN}", I'm going to get ${lowerA}`,
     `It ain't over until the ${lowerN} sings.`,
     `Let's talk more about ${lowerN}s. ${upperN}s that are ${lowerA}`,
     `Never look a ${lowerN} in the eye.`,
-    `Oh my god. Look at her ${lowerN}.`,
+    `Oh. My. God. Look at her ${lowerN}.`,
     `On Wednesdays I go bowling with a ${lowerN}.`,
     `Once upon a time there was a beautiful princess named ${proper}a.`,
     `Pancakes and ${lowerN}s. Mm-mm good.`,
     `Say it with me: ${proper}.`,
     `Tacos and ${lowerN}s.`,
     `Take my ${lowerN}! Just don't hurt me :(`,
+    `Take this ${lowerN}. It will help you along the way.`,
     `Thats nice but what about ${lowerN}s?`,
-    `The only good ${lowerN} is a ${lowerA} ${lowerN}`,
-    `The only thing that really matters is ${lowerN}s.`,
+    `The only good ${lowerN} is a ${lowerA} ${lowerN}.`,
+    `The only thing that really matters is ${lowerN}s. Really ${lowerA} ${lowerN}s`,
     `There's a ${lower} right behind you!`,
+    `That's what a ${lower} would say.`,
     `Today's secret word is "${proper}".`,
     `Would you still love me if I was ${lowerA}?`,
     `Would you still love me if I was a ${lowerN}?`
