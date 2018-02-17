37eafac513ae6e1a5106fb3ed4dd39186b67dfa7
Timothy Sullivan <tsullivan@elastic.co>
2018-02-16 22:20:31 -0700

    have it say a thing

--- server/keywords/get_responders.js
+++ server/keywords/get_responders.js
@@ -1,13 +1,14 @@
 function getResponders() {
   return {
-    help: require('./responders/help'),
-    repeat: require('./responders/repeat'),
-    joke: require('./responders/joke'),
-    superherofact: require('./responders/superherofact'),
-    starwarsfact: require('./responders/starwarsfact'),
-    random: require('./responders/random'),
     // just_dont: require('./responders/just_dont'),
     game: require('./responders/play'),
+    help: require('./responders/help'),
+    joke: require('./responders/joke'),
+    random: require('./responders/random'),
+    repeat: require('./responders/repeat'),
+    say: require('./responders/say'),
+    starwarsfact: require('./responders/starwarsfact'),
+    superherofact: require('./responders/superherofact'),
   };
 }
 
