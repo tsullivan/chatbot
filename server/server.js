472186334d8a2642bef13b91cbd2bf73f28c54a8
Timothy Sullivan <tsullivan@elastic.co>
2018-03-12 11:37:22 -0700

    lol bug

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
 const { apm: apmConfig } = require('../config');
 const apm = require('elastic-apm-node').start(apmConfig);
 
+const { join } = require('path');
 const express = require('express');
 const { initSession } = require('./session');
 const { initRoutes } = require('./routes');
 
 const app = express();
+const public = join(__dirname, '..', 'public');
 
-app.use(express.static(__dirname + '/public')); // home html page, static js
+app.use(express.static(public)); // home html page, static js
 app.use(apm.middleware.connect());
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(join(public, 'index.html'));
 });
 
 // init chat route
