0353d7bfc9ff958a5717eb65725a58fde3d71d83
Timothy Sullivan <tsullivan@elastic.co>
2018-07-15 16:32:53 -0700

    error handling

--- server/keywords/keyword_tester.js
+++ server/keywords/keyword_tester.js
@@ -3,6 +3,9 @@ const { getResponders } = require('./get_responders');
 function keywordTester(input, chat) {
   const responders = getResponders();
   for (const keyword in responders) {
+    if (responders[keyword] == null || responders[keyword] == null) {
+      throw new Error('Bad keyword responder: ' + keyword);
+    }
     const responder = new responders[keyword].KeywordResponder(input, chat);
     if (responder.inputMatches()) {
       return {
