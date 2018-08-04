14919bdaf329edc1f728e6efa2083d349879456f
Timothy Sullivan <tsullivan@elastic.co>
2018-08-04 00:53:18 -0700

    keyword test

--- test/keywords.test.js
+++ test/keywords.test.js
@@ -7,13 +7,63 @@ const agent = request.agent(app);
 const { handshake } = utilFactory(agent);
 
 describe('Keywords', () => {
-  test.skip('should give a requested joke', async () => {
+  test('should repeat', async () => {
     await handshake();
-
     const { statusCode, body } = await agent
       .post('/chat')
-      .send({ format: 'user', message: 'joke 5' });
+      .send({ format: 'user', message: 'repeat 450 💀' });
     expect(statusCode).toEqual(200);
     expect(body.message).toMatchSnapshot();
+  });
+
+  test('should help', async () => {
+    await handshake();
+    const { statusCode, body } = await agent
+      .post('/chat')
+      .send({ format: 'user', message: 'help' });
+    expect(statusCode).toEqual(200);
+    expect(body.message).toMatchSnapshot();
+  });
+
+  test('should say', async () => {
+    await handshake();
+    const { statusCode, body } = await agent
+      .post('/chat')
+      .send({ format: 'user', message: 'say mama say mama saw' });
+    expect(statusCode).toEqual(200);
+    expect(body.message).toMatchSnapshot();
+  });
+
+  test('should name', async () => {
+    await handshake();
+
+    (async () => {
+      const { statusCode, body } = await agent
+        .post('/chat')
+        .send({ format: 'user', message: 'name' });
+      expect(statusCode).toEqual(200);
+      expect(body.message).toMatchSnapshot();
+    })();
+    (async () => {
+      const { statusCode, body } = await agent
+        .post('/chat')
+        .send({ format: 'user', message: 'Mit' });
+      expect(statusCode).toEqual(200);
+      expect(body.message).toMatchSnapshot();
+    })();
+    (async () => {
+      const { statusCode, body } = await agent
+        .post('/chat')
+        .send({ format: 'user', message: 'name' });
+      expect(statusCode).toEqual(200);
+      expect(body.message).toMatchSnapshot();
+    })();
+    (async () => {
+      const { statusCode, body } = await agent
+        .post('/chat')
+        .send({ format: 'user', message: 'Tim' }); // reset
+      expect(statusCode).toEqual(200);
+      expect(body.message).toMatchSnapshot();
+    })();
   });
 });
