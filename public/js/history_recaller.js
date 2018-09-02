d52b7cada9e256e92eb3dd5f3de914744410adf7
Timothy Sullivan <tsullivan@elastic.co>
2018-09-01 22:01:10 -0700

    up and down arrow

--- public/js/history_recaller.js
+++ public/js/history_recaller.js
@@ -1,30 +1,38 @@
 window.HistoryRecaller = class {
   constructor() {
-    this._userMessages = [];
-    this.save();
+    const stored = localStorage.getItem('user_messages');
+    if (stored != null) {
+      this._userMessages = JSON.parse(localStorage.getItem('user_messages'));
+    } else {
+      this._userMessages = [];
+    }
+    this.resetPointer();
   }
 
   resetPointer() {
-    this._pointer = Math.max(0, this._userMessages.length - 1);
-  }
-
-  rewindPointer() {
-    this._pointer = Math.max(0, this._pointer - 1);
+    this._pointer = -1;
   }
 
   save() {
+    this._userMessages.length = Math.min(25, this._userMessages.length); // keep only up to 25 things
     this.resetPointer();
     localStorage.setItem('user_messages', JSON.stringify(this._userMessages));
   }
 
   addHistory(text) {
-    this._userMessages.push(text);
-    return this.save();
+    this._userMessages.unshift(text);
+    this.save();
+  }
+
+  getLaterText() {
+    this._pointer = Math.max(0, this._pointer - 1);
+    const lastMessage = this._userMessages[this._pointer];
+    return lastMessage ? lastMessage : '';
   }
 
   getEarlierText() {
-    const lastMessage = this._userMessages[this._pointer];
-    this.rewindPointer();
-    return lastMessage ? lastMessage : '';
+    this._pointer = Math.min(this._userMessages.length - 1, this._pointer + 1);
+    const nextMessage = this._userMessages[this._pointer];
+    return nextMessage ? nextMessage : '';
   }
 };
