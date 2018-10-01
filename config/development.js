11268b2e0b8eedcfce440ef57640caa5d312ce4d
Timothy Sullivan <tsullivan@elastic.co>
2018-09-30 17:25:55 -0700

    dev apm

--- config/development.js
+++ config/development.js
@@ -3,7 +3,6 @@ module.exports = {
   session_secret: 'YhqbEu2tVaT1v0bf7QHhT59Nv2JmpXFd',
   apm: {
     serviceName: 'chatbot-dev',
-    secretToken: 'something',
-    serverUrl: 'localhost:8200',
+    active: false,
   },
 };
