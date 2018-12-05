e51fcc6128446b7176161a480d6114fad57c6cf5
Timothy Sullivan <tsullivan@elastic.co>
2018-12-05 14:00:42 -0700

    docker/slack

--- config/docker.js
+++ config/docker.js
@@ -2,8 +2,12 @@ module.exports = {
   env: 'docker',
   session_secret: process.env.SESSION_SECRET,
   apm: {
-    serviceName: 'chatbot',
-    secretToken: process.env.APM_TOKEN,
+    serviceName: 'chatbot-docker',
+    secretToken: process.env.ELASTIC_APM_TOKEN,
     serverUrl: process.env.ELASTIC_APM_SERVER_URL,
+  },
+  slack: {
+    token: process.env.SLACK_BOT_TOKEN,
+    name: process.env.SLACK_BOT_NAME,
   },
 };
