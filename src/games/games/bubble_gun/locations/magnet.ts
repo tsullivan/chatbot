de3fb294c34027c631b958515c5184ccfc9ac126
Timothy Sullivan <tsullivan@elastic.co>
2019-01-21 19:03:34 -0700

    basement play through

--- src/games/games/bubble_gun/locations/magnet.ts
+++ src/games/games/bubble_gun/locations/magnet.ts
@@ -17,13 +17,11 @@ export class MagnetLocation extends Location {
   public getDescription(game: AdventureGame) {
     const stairsConditional = [
       snl`Perhaps if you explore a bit, you could find there is more to this store.`,
-      snl`Behind a leaning shelf is a wall with a small door. You can crouch
-      down to enter the door, and descend the well-lit stairs.`,
+      snl`Behind a leaning shelf is a wall with a small door. You can crouch down to enter the door, and descend the well-lit stairs.`,
     ];
-    return snl`There are lots and lots of metal shelves in here. The entire
-    surfaces of all the shelves have magnets stuck to them. The aisles and
+    return snl`There are lots and lots of metal shelves in here. The entire surfaces of all the shelves have magnets stuck to them. The aisles and
     aisles of shelves create a mysterious atmosphere, where anything could be
-    hiding. ${stairsConditional[this.stairsUncovered ? 0 : 1]}`;
+    hiding. ${stairsConditional[this.stairsUncovered ? 1 : 0]}`;
   }
 
   public setLocationKeywords(game: AdventureGame) {
@@ -34,11 +32,8 @@ export class MagnetLocation extends Location {
     if (this.stairsUncovered) {
       const desc = `Sneak into the small secret door on the wall behind the leaning shelf.`;
       this.addKeyword('SECRET_DOOR', desc, () => {
-        const pre = snl`The door leads directly to a case of stairs going down.
-        There are many stairs to walk down, and it takes time to get all the
-        way to the bottom. Finally, you make it to the bottom where there is a
-        room with a wide, open floor, and a very tall ceiling. It is a very
-        well-lit and clean place - it feels sterile.`;
+        const pre = snl`The door leads directly to a case of stairs going down. There are many stairs to walk down, and it takes time to get all the
+        way to the bottom. Finally, you make it to the bottom where there is a room with a wide, open floor, and a very tall ceiling.`;
         return this.followExit(DOWN, pre);
       });
     } else {
@@ -48,15 +43,11 @@ export class MagnetLocation extends Location {
         () => {
           this.stairsUncovered = true;
           const text = parajoin([
-            snl`The magnet store has the feel of a place where anything could be
-          hiding. It's time to get to the bottom of this mystery.`,
-            snl`You wander around some aisles, go around some corners, look
-          around. You're not exactly sure what you're looking for. You are
-          keeping your eyes open for anything that seems out of place in this
-          strange store.`,
-            snl`Just when you think maybe this is a normal magnet store after
-          all, you discover a small door on a wall. It is almost completely
-          hidden behind a shelf leaning against the wall.`,
+            snl`The magnet store has the feel of a place where anything could be hiding. It's time to get to the bottom of this mystery.`,
+            snl`You wander around some aisles, go around some corners, look around. You're not exactly sure what you're looking for. You are
+            keeping your eyes open for anything that seems out of place in this strange store.`,
+            snl`Just when you think maybe this is a normal magnet store after all, you discover a small door on a wall. It is almost completely
+            hidden behind a shelf leaning against the wall.`,
           ]);
           return new KeywordResponse({ text });
         },
--- src/games/games/bubble_gun/locations/magnet_basement.ts
+++ src/games/games/bubble_gun/locations/magnet_basement.ts
@@ -9,7 +9,7 @@ import { UP } from '../constants';
 
 const STAT_LOOMING = 'LOOMING';
 const STAT_EXPOSED = 'EXPOSED';
-const STAT_UNDEFINED = 'EXPOSED';
+const STAT_UNDEFINED = 'UNDEFINED';
 const STAT_IGNITED_TARGET1 = 'IGNITED_TARGET1';
 const STAT_IGNITED_TARGET2 = 'IGNITED_TARGET2';
 const STAT_ERROR = 'ERROR';
@@ -23,6 +23,7 @@ const MACHINE_CYCLES = [
 ];
 
 export class MagnetBasementLocation extends Location {
+  private canExaminePanel = true;
   private machineUncovered = false;
   private machineExtended = false;
   private machineCycles = MACHINE_CYCLES;
@@ -34,7 +35,7 @@ export class MagnetBasementLocation extends Location {
 
   public getDescription(game: AdventureGame) {
     const machineConditional = [
-      snl`It is a very well-lit and clean place - it feels sterile.`,
+      snl`It is a very well-lit and clean place - it almost feels sterile.`,
       snl`There is a lot of complicated machinery curled up in a really big hole in the floor. The room is a bit dusty now. It seems like this floor
       hasn't been opened up in a long time: maybe ever.`,
       snl`An enormous machine arm has extended out of the floor, crashed through the ceiling, and crashed through both the inside of and the roof
@@ -62,94 +63,76 @@ export class MagnetBasementLocation extends Location {
     this.addKeyword('UP', 'Go back to the magnet store', () => {
       return this.followExit(UP);
     });
-    this.addKeyword(
-      'EXAMINE_CONTROL_PANEL',
-      'Take a look at what this control panel has to offer.',
-      () => {
-        const text = snl`The panel has a small screen and a button of about the same size. The screen reads one message: \`STATUS: ${
-          this.machineStatus
-        }\`.
-        The button is labeled \`STATUS_CYCLE\`. There is another slightly larger screen on the control panel, which is a bird's-eye view of Bubble Gun World. It
-        shows targets on the Playground and the Bridge.`;
-        return new KeywordResponse({ text });
-      },
-    );
-
-    if (this.machineUncovered) {
-      this.addKeyword('STATUS_CYCLE', 'Press the STATUS_CYCLE button', () => {
-        // increment
-        this.machineCycleCurrent = Math.min(
-          this.machineCycleCurrent + 1,
-          this.machineCycles.length - 1,
-        );
-        // react to new status
-        let machineText;
-        switch (this.machineStatus) {
-          case STAT_EXPOSED:
-            machineText = parajoin([
-              snl`The bright florescent lights suddently shut off. Red alerting lights turn on and paint the large room with red light and long shadows.`,
-              snl`The floor starts to shake, and it becomes necessary for you to grab onto the guard rail to remain standing. The floor starts to
-              separate in the middle - opening like cargo doors - to reveal machinery beneath the floor.`,
-              snl`In the glow of the red light, you can still read the message on the control panel status screen. It has changed to: \`STATUS: ${
-                this.machineStatus
-              }\``,
-            ]);
-            break;
-          case STAT_UNDEFINED:
-            machineText = parajoin([
-              (machineText = snl`Gears in the machinery start to slowly turn, and parts of the machine start to unfold. First, metal arms and cranks
-              reach out of the separated floor, into the air, higher and higher.  It crashes the ceiling, and creates a new window into the Magnet
-              Store above. Magnets are wapping off the shelves and wapping onto the machine arm. But the machine arm still goes up and up and up.
-              It crashes out of the roof of the building, and then finally becomes still.`),
-              (machineText = snl`All the crashing has created a lot of debris, rubble and dust in the basement around you. But the
-              control panel is still working. Now the status screen reads: \`STATUS: ${
-                this.machineStatus
-              }\``),
-              (machineText = snl`It seems this part of the machine functionality has never been tested before. That would explain why the ceilings
-              and roof wasn't destroyed when you got into town.`),
-              (machineText = snl`The Playground and the Bridge on the map of the control panel have their targets blinking.`),
-            ]);
-            break;
-          case STAT_IGNITED_TARGET1:
-            machineText = parajoin([
-              snl`Suddenly, a loud noise blasts your ears: THUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUD. The noise is coming from
-              high above you, outside the building. After about a minute, the noise stops.`,
-              snl`The Playground's icon on the map changed from a target to an X.`,
-            ]);
-            break;
-          case STAT_IGNITED_TARGET2:
-            machineText = parajoin([
-              snl`Again a loud noise blasts your ears: THUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUD. It's just as loud and
-              long as last time. This time, when the THUDs stop, you hear some clicks, and then that's it.`,
-              snl`The Bridge's icon on the map changed from a target to an X.`,
-            ]);
-            break;
-          default:
-            machineText = parajoin([
-              snl`When you push the button now, the machine shakes a little bit and makes some clunking sounds, but nothing happens. It seems to
-              be out of ammunition.`,
-            ]);
-        }
-
-        // report
-        return new KeywordResponse({ text: machineText });
-      });
-    } else {
+    if (this.canExaminePanel) {
       this.addKeyword(
-        'EXPLORE',
-        'See what could be hiding in this strange shop.',
+        'EXAMINE_CONTROL_PANEL',
+        'Take a look at what this control panel has to offer.',
         () => {
-          this.machineUncovered = true;
-          const text = parajoin([
-            snl`The magnet store has the feel of a place where anything could be hiding. It's time to get to the bottom of this mystery.`,
-            snl`You wander around some aisles, go around some corners, look around. You're not exactly sure what you're looking for. You are
-            keeping your eyes open for anything that seems out of place in this strange store.`,
-            snl`Just when you think maybe this is a normal magnet store after all, you discover a small door on a wall. It is almost completely
-            hidden behind a shelf leaning against the wall.`,
-          ]);
+          this.canExaminePanel = false;
+          const text = snl`The panel has a small screen and a button of about the same size. The screen reads one message: **STATUS: ${this.machineStatus}**.
+          The button is labeled **STATUS_CYCLE**. There is another slightly larger screen on the control panel: a map showing all of Bubble Gun
+          World. It shows targets on the Playground and the Bridge.`;
           return new KeywordResponse({ text });
         },
       );
     }
+
+    this.addKeyword('STATUS_CYCLE', 'Press the STATUS_CYCLE button', () => {
+      // increment
+      this.machineCycleCurrent = Math.min(
+        this.machineCycleCurrent + 1,
+        this.machineCycles.length - 1,
+      );
+      // react to new status
+      let machineText;
+      switch (this.machineStatus) {
+        case STAT_EXPOSED:
+          machineText = parajoin([
+            snl`The bright florescent lights suddently shut off. Red alerting lights turn on and paint the large room with red light and long
+            shadows.`,
+            snl`The floor starts to shake, and it becomes necessary for you to grab onto the guard rail to remain standing. The floor starts to
+            separate in the middle - opening like cargo doors - to reveal machinery beneath the floor.`,
+            snl`In the glow of the red light, you can still read the message on the control panel status screen. It has changed to: **STATUS: ${this.machineStatus}**`,
+          ]);
+          break;
+        case STAT_UNDEFINED:
+          machineText = parajoin([
+            (machineText = snl`Gears in the machinery start to slowly turn, and parts of the machine start to unfold. First, metal arms and cranks
+            reach out of the separated floor, into the air, higher and higher. It crashes the ceiling, and creates a new window into the Magnet
+            Store above. Magnets are wapping off the shelves and wapping onto the machine arm. But the machine arm still goes up and up and up.
+            It crashes out of the roof of the building, and then finally becomes still.`),
+            (machineText = snl`All the crashing has created a lot of debris, rubble and dust in the basement around you. But the
+            control panel is still working. Now the status screen reads: **STATUS: ${this.machineStatus}**`),
+            (machineText = snl`It seems this part of the machine functionality has never been tested before. That would explain why the ceilings
+            and roof wasn't destroyed when you got into town.`),
+            (machineText = snl`The Playground and the Bridge on the map of the control panel have their targets blinking.`),
+          ]);
+          break;
+        case STAT_IGNITED_TARGET1:
+          // TODO update game state: playground
+          machineText = parajoin([
+            snl`Suddenly, a loud noise blasts your ears: THUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUD. The noise is coming from
+            high above you, outside the building. After about a minute, the noise stops.`,
+            snl`The Playground's icon on the map changed from a target to an X.`,
+          ]);
+          break;
+        case STAT_IGNITED_TARGET2:
+          // TODO update game state: bridge
+          machineText = parajoin([
+            snl`Again a loud noise blasts your ears: THUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUDUD. It's just as loud and
+            long as last time. This time, when the THUDs stop, you hear some clicks, and then that's it.`,
+            snl`The Bridge's icon on the map changed from a target to an X.`,
+          ]);
+          break;
+        default:
+          machineText = parajoin([
+            snl`When you push the button now, the machine shakes a little bit and makes some clunking sounds, but nothing happens. It seems to
+            be out of ammunition.`,
+          ]);
+      }
+
+      // report
+      return new KeywordResponse({ text: machineText });
+    });
   }
 }
