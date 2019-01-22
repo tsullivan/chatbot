e10773a9cb6f97f604659be7bb7f5846fb840a0b
Timothy Sullivan <tsullivan@elastic.co>
2019-01-21 18:44:54 -0700

    no apm for development

--- config/development.ts
+++ config/development.ts
@@ -2,8 +2,7 @@ export const env = 'development';
 export const sessionSecret = process.env.SESSION_SECRET;
 export const apm = {
   serviceName: 'chatbot-development',
-  secretToken: process.env.APM_TOKEN,
-  serverUrl: process.env.ELASTIC_APM_SERVER_URL,
+  active: false,
 };
 export const slack = {
   token: process.env.SLACK_BOT_TOKEN,
