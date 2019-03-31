b2dc30f3a955eca7d1d213a59669ce345cb1d356
Timothy Sullivan <tsullivan@elastic.co>
2019-03-30 23:26:13 -0700

    fix tests

--- src/slackbot/events/index.ts
+++ src/slackbot/events/index.ts
@@ -3,7 +3,6 @@ import { SlackBot } from '../slackbot';
 import { onMessageFactory } from './on_message';
 
 export async function initRTMEvents(slackBot: SlackBot, rtm: RTMClient) {
-  const log = slackBot.getLogger(['slack', 'rtm', 'events']);
   rtm.start();
 
   // Do hello message
@@ -13,25 +12,6 @@ export async function initRTMEvents(slackBot: SlackBot, rtm: RTMClient) {
     slackBot.getSlackChannel(),
   );
 
+  // @ts-ignore
   rtm.on('message', onMessageFactory(slackBot, rtm));
-
-  /* debug */
-  rtm.on('hello', event => {
-    log.json(['hello'], event);
-  });
-  rtm.on('im_close', event => {
-    log.json(['im_close'], event);
-  });
-  rtm.on('im_created', event => {
-    log.json(['im_craeaed'], event);
-  });
-  rtm.on('im_history', event => {
-    log.json(['im_history'], event);
-  });
-  rtm.on('im_marked', event => {
-    log.json(['im_marked'], event);
-  });
-  rtm.on('im_open', event => {
-    log.json(['im_open'], event);
-  });
 }
