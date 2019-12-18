84df4ce9b3378098c8557ae28c96e94f9e178c83
Timothy Sullivan <tsullivan@elastic.co>
2019-12-18 10:59:09 -0700

    upgrade more deps

--- test/utils.ts
+++ test/utils.ts
@@ -5,7 +5,9 @@ import { getServer } from '../src/web';
 export type HandshakeFn = () => Promise<void>;
 export type SendFn = (message: string) => Promise<request.Response>;
 export type CheckRespFn = (resps: any[]) => void;
-export type BeforeAllFn = (bot: Bot) => Promise<{
+export type BeforeAllFn = (
+  bot: Bot
+) => Promise<{
   agent: request.SuperTest<request.Test>;
   handshake: HandshakeFn;
 }>;
@@ -37,24 +39,22 @@ export const utilFactory: UtilFactory = () => {
         .post('/chat')
         .send({ format: 'syn', message: 'HELO' })
         .expect(200)
-        .then(
-          (res: any): Promise<void> => {
-            expect(res.body.format).toEqual('plain');
-            expect(res.body.name).toEqual('Beep Beep Beep');
+        .then((res: any) => {
+          expect(res.body.format).toEqual('plain');
+          expect(res.body.name).toEqual('Beep Beep Beep');
 
-            if (res.body.message === 'Hello! What is your name?') {
-              return this.agent
-                .post('/chat')
-                .send({ format: 'user', message: 'Tim' })
-                .expect(200)
-                .then((ret: any) => {
-                  expect(ret.body.message).toEqual('Hello, Tim! Nice to meet you.');
-                });
-            }
-
-            return expect(res.body.message).toEqual('Hello again, Tim!');
+          if (res.body.message === 'Hello! What is your name?') {
+            return this.agent
+              .post('/chat')
+              .send({ format: 'user', message: 'Tim' })
+              .expect(200)
+              .then((ret: any) => {
+                expect(ret.body.message).toEqual('Hello, Tim! Nice to meet you.');
+              });
           }
-        );
+
+          return expect(res.body.message).toEqual('Hello again, Tim!');
+        });
     },
 
     send: (message: string) => {
