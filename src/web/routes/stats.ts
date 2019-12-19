b8d957cf3d40b1eb3c74bce6938351dfa02db9db
Timothy Sullivan <tsullivan@elastic.co>
2019-12-18 17:02:00 -0700

    add session info to route

--- src/web/routes/stats.ts
+++ src/web/routes/stats.ts
@@ -3,6 +3,9 @@ import { Bot } from '../..//bot';
 
 export function statsRoute(app: express.Application, bot: Bot) {
   app.get('/stats', (req: express.Request, res) => {
-    res.json(bot.getMetrics(req));
+    res.json({
+      session: req.session.chat,
+      metrics: bot.getMetrics(req),
+    });
   });
 }
